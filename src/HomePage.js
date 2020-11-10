7c82161c5d99e6ac0f456df01de24a01e60335bc
Melanie <melaniechele@gmail.com>
2020-11-09 20:06:17 -0500

    small changes

--- src/HomePage.js
+++ src/HomePage.js
@@ -9,7 +9,7 @@ export default function HomePage(props) {
   };
   return (
     <>
-      <h2>Your favorite food, delivered while coding</h2>
+      <h2>Your favorite pizza, here.</h2>
       <button className='homePage-button' onClick={navToPizzaForm}>
         Pizza?
       </button>
